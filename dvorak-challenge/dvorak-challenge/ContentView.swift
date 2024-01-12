6a35429a060cf9da420322655cdeb2b5125084b2
Henrique Akiyoshi Eguchi <henrique.eguchi@outlook.com>
2024-01-12 20:18:28 -0300

    Initial project

---
+++ dvorak-challenge/dvorak-challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dvorak-challenge
+//
+//  Created by Henrique Akiyoshi Eguchi on 12/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
