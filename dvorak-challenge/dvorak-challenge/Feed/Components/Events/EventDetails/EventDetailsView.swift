a375cfbc9e5ca394f903fe4660aa8f3b3cc0b7c2
Henrique Akiyoshi Eguchi <henrique.eguchi@outlook.com>
2024-01-13 15:19:59 -0300

    Add EventInformationView UI component for Details; Organize folder structure for event and service

---
+++ dvorak-challenge/dvorak-challenge/Feed/Components/Events/EventDetails/EventDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  EventDetailsView.swift
+//  dvorak-challenge
+//
+//  Created by Henrique Akiyoshi Eguchi on 13/01/24.
+//
+
+import SwiftUI
+
+struct EventDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventDetailsView()
+}
