6c9a6c9d1930fe5a05df1b83fbb682765904fc70
Henrique Akiyoshi Eguchi <henrique.eguchi@outlook.com>
2025-01-15 20:00:49 -0300

    build: Initial project structure

---
+++ bipa-ios-test/bipa_ios_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  bipa_ios_testApp.swift
+//  bipa-ios-test
+//
+//  Created by Henrique Akiyoshi Eguchi on 15/01/25.
+//
+
+import SwiftUI
+
+@main
+struct bipa_ios_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
