6702931cf7feff42c40258a880e71cb88cd595d2
Henrique Akiyoshi Eguchi <henrique.eguchi@outlook.com>
2025-01-15 20:09:02 -0300

    feat: Add initial TopNode API mappings

---
+++ bipa-ios-test/TopNodesListByConnectivity/Service/TopNodeMapper.swift
@@ -0,0 +1,24 @@
+//
+//  TopNodeMapper.swift
+//  bipa-ios-test
+//
+//  Created by Henrique Akiyoshi Eguchi on 15/01/25.
+//
+
+final class TopNodeMapper {
+    struct TopNode: Decodable {
+        let publicKey: String
+        let alias: String
+        let channels: Int
+        let capacity: Int
+        let firstSeen: Int
+        let updatedAt: Int
+        let city: LocatedName?
+        let country: LocatedName?
+    }
+
+    struct LocatedName: Decodable {
+        let en: String
+        let ptBR: String?
+    }
+}
